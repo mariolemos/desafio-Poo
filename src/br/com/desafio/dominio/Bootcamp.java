9307ad198240014dcdc0e33790aca37818df7d15
lemos <lemos>
2024-06-23 08:31:58 -0300

    Implementado os métodos inscreverBootcamp, progredir e calcularXp

--- src/br/com/desafio/dominio/Bootcamp.java
+++ src/br/com/desafio/dominio/Bootcamp.java
@@ -1,7 +1,10 @@
 package br.com.desafio.dominio;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class Bootcamp {
 
@@ -9,6 +12,8 @@ public class Bootcamp {
     private String descricai;
     private final LocalDate dataInicial = LocalDate.now();
     private final LocalDate dataFinal = dataInicial.plusDays(45);
+    private Set<Dev> devsInscritos = new HashSet<>();
+    private Set<Conteudo> conteudos = new LinkedHashSet<>();
 
     public String getNome() {
         return nome;
@@ -32,6 +37,22 @@ public class Bootcamp {
 
     public LocalDate getDataFinal() {
         return dataFinal;
+    }
+
+    public Set<Dev> getDevsInscritos() {
+        return devsInscritos;
+    }
+
+    public void setDevsInscritos(Set<Dev> devsInscritos) {
+        this.devsInscritos = devsInscritos;
+    }
+
+    public Set<Conteudo> getConteudos() {
+        return conteudos;
+    }
+
+    public void setConteudos(Set<Conteudo> conteudos) {
+        this.conteudos = conteudos;
     }
 
     @Override
--- src/br/com/desafio/dominio/Dev.java
+++ src/br/com/desafio/dominio/Dev.java
@@ -2,6 +2,7 @@ package br.com.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -11,14 +12,24 @@ public class Dev {
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
     public void inscreverBootcamp(Bootcamp bootcamp) {
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
 
     }
 
     public void progredir() {
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if(conteudo.isPresent()) {
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        }else {
+            System.out.println();
+        }
 
     }
 
-    public void calcularTiotalXp() {
+    public double calcularTotalXp() {
+        return this.conteudosConcluidos.stream().mapToDouble(conteudo -> conteudo.calcularXp()).sum();
 
     }
 
@@ -45,6 +56,7 @@ public class Dev {
     public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
         this.conteudosConcluidos = conteudosConcluidos;
     }
+
 
     @Override
     public boolean equals(Object o) {
